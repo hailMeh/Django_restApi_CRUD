e6d74ce433c0b8f6362c7a90608a9453fd30fbbd
hailMeh <Sultanhabibi@mail.ru>
2022-04-22 13:34:44 +0300

    initial

---
+++ CRUD_django_rest/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('posts.urls'))
+    # Версия апи, каждая версия предоставляет разный json, делать обновление json лучше через новую версию, протестировать и сделать ее мэйном
+]
---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from .serializers import PostSerializer
+from rest_framework import generics
+from .models import Post
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
